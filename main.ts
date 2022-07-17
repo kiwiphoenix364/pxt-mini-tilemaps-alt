c1511a35394a13233584f20aa4a1387ec926660b
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-07-16 21:40:30 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -48,16 +48,16 @@ game.onUpdate(function () {
             curx = mySprite.x
             cury = mySprite.y
             mySprite.setPosition(sprites1.readDataNumber(mySprite, "prevx"), sprites1.readDataNumber(mySprite, "prevy"))
-            repeat = Math.abs(curx - sprites1.readDataNumber(mySprite, "prevx")) + Math.abs(cury - sprites1.readDataNumber(mySprite, "prevy"))
+            repeat = (Math.abs(curx - sprites1.readDataNumber(mySprite, "prevx")) + Math.abs(cury - sprites1.readDataNumber(mySprite, "prevy"))) * 2
             canx = 1
             cany = 1
-            for (let index = 0; index <= repeat; index++) {
-                if (repeat > 0) {
+            if (repeat > 0) {
+                for (let index = 0; index <= repeat; index++) {
                     if (canx == 1) {
                         mySprite.x += (curx - sprites1.readDataNumber(mySprite, "prevx")) / repeat
                         for (let index2 = 0; index2 <= TileCollisionArrayX.length; index2++) {
                             if (canx == 1) {
-                                if (TileCollisionArrayX[index2] - mySprite.left >= 0 && TileCollisionArrayX[index2] - mySprite.left < mySprite.width && (TileCollisionArrayY[index2] - mySprite.top >= 0 && TileCollisionArrayY[index2] - mySprite.top < mySprite.height)) {
+                                if ((TileCollisionArrayX[index2] - mySprite.left + 1 > 0 && TileCollisionArrayX[index2] - mySprite.left - 1 < mySprite.width) && (TileCollisionArrayY[index2] - mySprite.top + 1 > 0 && TileCollisionArrayY[index2] - mySprite.top - 1 < mySprite.height)) {
                                     if (mySprite.image.getPixel(TileCollisionArrayX[index2] - mySprite.left, TileCollisionArrayY[index2] - mySprite.top) != 0) {
                                         mySprite.x += 0 - (curx - sprites1.readDataNumber(mySprite, "prevx")) / repeat
                                         canx = 0
@@ -70,7 +70,7 @@ game.onUpdate(function () {
                         mySprite.y += (cury - sprites1.readDataNumber(mySprite, "prevy")) / repeat
                         for (let index22 = 0; index22 <= TileCollisionArrayY.length; index22++) {
                             if (cany == 1) {
-                                if (TileCollisionArrayX[index22] - mySprite.left >= 0 && TileCollisionArrayX[index22] - mySprite.left < mySprite.width && (TileCollisionArrayY[index22] - mySprite.top >= 0 && TileCollisionArrayY[index22] - mySprite.top < mySprite.height)) {
+                                if ((TileCollisionArrayX[index22] - mySprite.left + 1 > 0 && TileCollisionArrayX[index22] - mySprite.left - 1 < mySprite.width) && (TileCollisionArrayY[index22] - mySprite.top + 1 > 0 && TileCollisionArrayY[index22] - mySprite.top - 1 < mySprite.height)) {
                                     if (mySprite.image.getPixel(TileCollisionArrayX[index22] - mySprite.left, TileCollisionArrayY[index22] - mySprite.top) != 0) {
                                         mySprite.y += 0 - (cury - sprites1.readDataNumber(mySprite, "prevy")) / repeat
                                         cany = 0
